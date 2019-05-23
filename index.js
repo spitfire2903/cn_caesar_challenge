80d94dc59770bece0e9694466565c5c015773a4b
Ricardo N. de Miranda <nm.ricardo@gmail.com>
2019-05-23 20:07:04 -0300

    Debugging

--- index.js
+++ index.js
@@ -12,13 +12,12 @@ let crypto = null;
 
 getCryptoText = async () => {
     let jsonText = await new Promise((res, rej) => {
-        request.get(URL_GENERATE_DATA, (response, error, body) => {
+        request.get(URL_GENERATE_DATA, (error, response, body) => {
             if (error) {
                 console.error(error);
                 rej(error);
             }
 
-            console.info(body);
             res(body);
         });
     });
@@ -26,8 +25,55 @@ getCryptoText = async () => {
     return JSON.parse(jsonText);
 }
 
+isCharacter = (string) => {
+    return (/[a-z]/i).test(string);
+}
+
 decryptText = (text, diff) => {
-    return text;
+    return text.split('').map((char) => {
+        return decryptChar(char, diff);
+    }).join('');
+}
+
+decryptChar = (char, diff) => {
+    const INDEX_FIRST_CHAR = 97;
+    const INDEX_LAST_CHAR = 122;
+    const MAX_SIZE = 1 + (INDEX_LAST_CHAR - INDEX_FIRST_CHAR);
+
+    console.log(' > Char: '+char);
+    console.log(' > Diff: '+diff);
+
+    if (!isCharacter(char)) {
+        console.log(' > Char is not character');
+        return char;
+    }
+
+    let charCode = char.charCodeAt(0);
+    console.log(' > Charcode: '+charCode);
+
+    let position = charCode - diff;
+
+    console.log(' > Possible position: '+position);
+
+    if (position < INDEX_FIRST_CHAR) {
+        console.log(' > Position < FIRST_CHAR ');
+
+        position = INDEX_FIRST_CHAR - position;
+
+        console.log(' > Diff above the FIRST_CHAR: '+position);
+
+        position = position % MAX_SIZE;
+
+        console.log(' > Position to sub from LAST_CHAR: '+position);
+
+        position = INDEX_LAST_CHAR - position;
+
+        console.log(' > Final position: '+position);
+    }
+
+    console.log(' ');
+
+    return String.fromCharCode(position);
 }
 
 sendDecryptedText = async (text, file) => {
@@ -41,7 +87,7 @@ sendDecryptedText = async (text, file) => {
             }
         }
 
-        request.post(URL_SUBMIT_SOLUTION, options, (response, error, body) => {
+        request.post(URL_SUBMIT_SOLUTION, options, (error, response, body) => {
             if (error) {
                 console.error(error);
                 rej(error);
@@ -55,16 +101,17 @@ sendDecryptedText = async (text, file) => {
     return JSON.parse(jsonText);
 }
 
-writeFile = (data) => {
+writeData = (data) => {
     const file = fs.writeFileSync(FILENAME, data);
 
     return file;
 }
 
-
 main = async () => {
     let cryptoJson = await getCryptoText();
     let decryptedText = decryptText(cryptoJson.cifrado, cryptoJson.numero_casas);
+
+    console.warn('*** ' + decryptedText);
 
     cryptoJson.decifrado = decryptedText;
     cryptoJson.resumo_criptografico = sha1(decryptedText);
